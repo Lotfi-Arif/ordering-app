8a9e4371d0d41b616ebfbeb85559ed3fab54b3bf
Lotfi Arif <lotfiarif@Lotfis-Air.realtek>
2022-09-12 23:57:11 +0800

    Added dockerfile to orders and .env in all packages

--- apps/orders/src/orders.module.ts
+++ apps/orders/src/orders.module.ts
@@ -1,10 +1,25 @@
 import { Module } from '@nestjs/common';
 import { OrdersController } from './orders.controller';
 import { OrdersService } from './orders.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { PrismaModule, PrismaService } from 'nestjs-prisma';
+import {
+  ApolloFederationDriver,
+  ApolloFederationDriverConfig,
+} from '@nestjs/apollo';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloFederationDriverConfig>({
+      driver: ApolloFederationDriver,
+      cors: false,
+      autoSchemaFile: true,
+    }),
+    PrismaModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
   controllers: [OrdersController],
-  providers: [OrdersService],
+  providers: [OrdersService, PrismaService],
 })
 export class OrdersModule {}
